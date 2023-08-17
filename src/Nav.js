65531d0f89ab58292616e4001385f42ed03f43ec
David Ogwuche <133539148+codedvd@users.noreply.github.com>
2023-08-17 04:50:23 +0100

    Managing Dependencies and Hosting to Firebase

---
+++ src/Nav.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        };
+        window.addEventListener("scroll", scrollHandler);
+
+        return () => {
+            window.removeEventListener("scroll", scrollHandler);
+        };
+    }, []);
+
+
+  return (
+    <div className={`Nav ${show && "nav__black"}`}>
+        <img 
+            className="nav__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Netflix Logo"
+        />
+
+        <img 
+            className="nav__avatar"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="Netflix Logo"
+        />
+    </div>
+  )
+}
+
+export default Nav
